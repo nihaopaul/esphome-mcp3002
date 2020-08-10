e4c8cff854d441865453a18d5b07ae7950e27925
Paul <nihaopaul@gmail.com>
2020-08-10 21:07:31 +0800

    mcp3002 adc for esphome
    mcp3002 adc for esphome - initial work in the headers, move to configurable

---
+++ mcp3002.h
@@ -0,0 +1,47 @@
+#include "esphome.h"
+
+
+class MCP3002 : public PollingComponent, public spi::SPIDevice<spi::BIT_ORDER_MSB_FIRST, spi::CLOCK_POLARITY_LOW, spi::CLOCK_PHASE_LEADING, spi::DATA_RATE_1KHZ> {
+  public:
+
+    Sensor *channel_0 = new Sensor();
+    Sensor *channel_1 = new Sensor();
+    
+    MCP3002() : PollingComponent(1000) { }
+
+    void setup() override {
+      SPI.begin();
+      pinMode(CS_PIN, OUTPUT);
+      
+    }
+
+    void update() override {
+
+      word chan0 = read_channel(CHANNEL0);
+      channel_0->publish_state(chan0);
+
+      word chan1 = read_channel(CHANNEL1);
+      channel_1->publish_state(chan1);
+    }
+
+  protected:
+    const int CS_PIN = D8;
+    const uint8_t CHANNEL0 = 0b11000000;
+    const uint8_t CHANNEL1 = 0b11100000;
+    word read_channel(uint8_t cmd) {
+
+      digitalWrite (CS_PIN, LOW);
+      byte msb, lsb;
+      word assembled;
+
+      msb = SPI.transfer(cmd);
+      lsb = SPI.transfer(0);
+
+      digitalWrite (CS_PIN, HIGH);
+      SPI.endTransaction();
+
+      return word(msb, lsb);
+
+    }
+  
+};
